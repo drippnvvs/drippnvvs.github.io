a2d2a413b39c0d514cebc2fbed42711ac3829bf7
dr1ppnvv5 <dr1ppnvv5@gmail.com>
2017-05-12 23:04:12 -0400

    test

--- js/drippnvvs.20170512230257.js
+++ js/drippnvvs.20170512230257.js
@@ -5,33 +5,35 @@ function em1(){var c="esjqqowwtAhnbjm/dpn";var addr="mailto:";for(var i=0;i<c.le
 
 $(document).ready(function() {
 r=function(){dpi=window.devicePixelRatio;$('.js-2').attr('src', (dpi>1) ? 'images/drippn-300.jpg' : 'images/drippn-150.jpg');
-$('.js-3').attr('src', (dpi>1) ? 'images/com.atebits.tweetie2-large-176.png' : 'images/com.atebits.tweetie2-large-88.png');
-$('.js-4').attr('src', (dpi>1) ? 'images/com.reddit.reddit-large-186.png' : 'images/com.reddit.reddit-large-93.png');
-$('.js-5').attr('src', (dpi>1) ? 'images/drippn-170.jpg' : 'images/drippn-85.jpg');
+$('.js-3').attr('src', (dpi>1) ? 'images/com.atebits.tweetie2-large-176-4.png' : 'images/com.atebits.tweetie2-large-88-4.png');
+$('.js-4').attr('src', (dpi>1) ? 'images/com.yourcompany.ppclient-large-158-4.png' : 'images/com.yourcompany.ppclient-large-79-4.png');
+$('.js-5').attr('src', (dpi>1) ? 'images/com.reddit.reddit-large-186.png' : 'images/com.reddit.reddit-large-93.png');
+$('.js-6').attr('src', (dpi>1) ? 'images/drippn-170.jpg' : 'images/drippn-85.jpg');
 $('.js-8').attr('src', (dpi>1) ? 'images/image-572.jpg' : 'images/image-286.jpg');
-$('.js-9').attr('src', (dpi>1) ? 'images/screen-shot-2017-03-31-at-11.49.29-pm-1014.jpg' : 'images/screen-shot-2017-03-31-at-11.49.29-pm-507.jpg');
-$('.js-11').attr('src', (dpi>1) ? 'images/dd-432.png' : 'images/dd-216.png');
-$('.js-13').attr('src', (dpi>1) ? 'images/com.hammerandchisel.discord-large-422.png' : 'images/com.hammerandchisel.discord-large-211.png');
+$('.js-9').attr('src', (dpi>1) ? 'images/wip3-654.png' : 'images/wip3-327.png');
+$('.js-11').attr('src', (dpi>1) ? 'images/screen-shot-2017-03-31-at-11.49.29-pm-1014.jpg' : 'images/screen-shot-2017-03-31-at-11.49.29-pm-507.jpg');
+$('.js-13').attr('src', (dpi>1) ? 'images/dd-432.png' : 'images/dd-216.png');
+$('.js-15').attr('src', (dpi>1) ? 'images/com.hammerandchisel.discord-large-422.png' : 'images/com.hammerandchisel.discord-large-211.png');
 
-$('.js-17').attr('src', (dpi>1) ? 'images/com.atebits.tweetie2-large-176.png' : 'images/com.atebits.tweetie2-large-88.png');
-$('.js-18').attr('src', (dpi>1) ? 'images/com.google.inbox-large-202.png' : 'images/com.google.inbox-large-101.png');
-$('.js-19').attr('src', (dpi>1) ? 'images/com.reddit.reddit-large-186.png' : 'images/com.reddit.reddit-large-93.png');};
+$('.js-19').attr('src', (dpi>1) ? 'images/com.atebits.tweetie2-large-176-2.png' : 'images/com.atebits.tweetie2-large-88-2.png');
+$('.js-20').attr('src', (dpi>1) ? 'images/com.google.inbox-large-202.png' : 'images/com.google.inbox-large-101.png');
+$('.js-21').attr('src', (dpi>1) ? 'images/com.reddit.reddit-large-186-2.png' : 'images/com.reddit.reddit-large-93-2.png');};
 if(!window.HTMLPictureElement){r();}
 (function(){$('a[href^="#"]').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
 var wl = new woolite();
 wl.init();
 wl.addAnimation($('.js')[0], "1.00s", "0.00s", 1, 100);
-wl.addAnimation($('.js-6')[0], "1.00s", "0.00s", 1, 100);
 wl.addAnimation($('.js-7')[0], "1.00s", "0.00s", 1, 100);
 wl.addAnimation($('.js-8')[0], "1.00s", "0.00s", 1, 100);
-wl.addAnimation($('.js-9')[0], "1.00s", "0.00s", 1, 100);
 wl.addAnimation($('.js-10')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-11')[0], "1.00s", "0.00s", 1, 100);
 wl.addAnimation($('.js-12')[0], "1.00s", "0.00s", 1, 100);
 wl.addAnimation($('.js-14')[0], "1.00s", "0.00s", 1, 100);
 wl.addAnimation($('.js-16')[0], "1.00s", "0.00s", 1, 100);
-wl.addAnimation($('.js-17')[0], "1.00s", "0.00s", 1, 100);
 wl.addAnimation($('.js-18')[0], "1.00s", "0.00s", 1, 100);
 wl.addAnimation($('.js-19')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-20')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-21')[0], "1.00s", "0.00s", 1, 100);
 wl.start();
 
 });
